e15c9151b0aabce0aa0c96d636600438316f196f
Sebastian Vidal Aedo <sebastian@intercode.cl>
2022-09-07 10:32:57 -0400

    initial commit

---
+++ src/views/Index/IndexPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "animate.css";
+import hero from '../../assets/images/referencia.png'
+
+const IndexPage = () => {
+  return (
+    <></>
+  );
+};
+
+export default IndexPage;
