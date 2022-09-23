239c08163324c767627ad19832274b00699a3905
Carlos Araya Salfate <carlosarayasalfate@gmail.com>
2022-09-23 16:59:55 -0300

    revision de las rutas

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react';
 
-import { Route, Routes } from "react-router-dom";
-import { Suspense } from "react";
-
-const DefaultLayout = React.lazy(() => import("../layout/DefaultLayout"));
-const IndexPage = React.lazy(() => import("../views/Index/IndexPage"));
+const DefaultLayout = React.lazy(() => import('../layout/DefaultLayout'));
+//const IndexPage = React.lazy(() => import('../views/Index/IndexPage'));
 
 function App() {
   return (
+    
     <Suspense>
       <Routes>
-        <Route path="/" element={<DefaultLayout />}>
-          <Route index element={<IndexPage />} />
+        <Route path="/" element={<DefaultLayout />} >
+          {/* <Route index element={<IndexPage />} /> */}
         </Route>
       </Routes>
     </Suspense>
+
   );
 }
 
