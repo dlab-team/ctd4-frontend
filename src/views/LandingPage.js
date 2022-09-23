326b014d0de79eb47befd81bc450852d9dbcb5fb
andrelealr <100944239+andrelealr@users.noreply.github.com>
2022-09-23 12:43:01 -0300

    feature: add LandingPage.js

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,20 +1,14 @@
-import { Landing1 } from "../components/Landing1";
-import Landing3 from "../components/Landing3";
-import { LandingPage2 } from "../components/LandingPage2";
-
+import { Landing1 } from '../components/Landing1';
+import Landing3 from '../components/Landing3';
+import { LandingPage2 } from '../components/LandingPage2';
 
 export function LandingPage() {
   return (
     <>
-      <div className="text-zinc-100">
-
-      </div>
+      <div className="text-zinc-100"></div>
       <Landing1 />
       <LandingPage2 />
       <Landing3 />
-
     </>
-  )
-
+  );
 }
-
