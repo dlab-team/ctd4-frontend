f9be05e65e22689dceb7c4995e944388d4acc74a
vverac <viverace@gmail.com>
2022-09-23 08:18:59 -0300

    feacture: componentes LandingPage2 CardsLandingpage2 para Landing ppal.

---
+++ src/components/LandingPage2.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import CardsLandingPage2 from './CardsLandingPage2'
+
+
+const LandingPage2 = () => {
+  return (
+    <>
+      <div
+        className="flex flex-row place-content-center justify-around content-center items-center pb-[84px] "
+        id="program"
+      >
+        <div className="flex flex-col items-center box-border pl-[134px] p-3">
+
+          <h1 className="font-[900] md:text-[48px] mt-[20px] text-[#2c3142] mb-[9.5px] ml-[20px] md:ml-0">
+            DEVsafio <br />
+            como cambiamos<br />
+            el mundo TI
+          </h1>
+
+        </div>
+        <div className="box-border pr-[134px]  p-3">
+          <iframe
+            className=" md:w-[627px] md:h-[423px]"
+            src="https://youtube.com/embed/z5NPi7WrViU"
+            frameborder="0"
+            allow="autoplay; encrypted-media"
+            allowfullscreen
+            title="video"
+          />{" "}
+        </div>
+      </div>
+      <CardsLandingPage2 />
+    </>
+  )
+}
+
+export default LandingPage2
--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,6 +1,8 @@
 
 
 export function LandingPage() {
-  return (<><h1 className="text-4xl text-primary">Landing Page...</h1></>)
+  return (
+    <>
+    </>)
 }
 
