8418517d313c9be782ff3c10d0da3309a24b5b81
andrelealr <100944239+andrelealr@users.noreply.github.com>
2022-09-27 17:48:51 -0300

    add vista mobil

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,41 +5,32 @@ export function Footer() {
 
     return (
 
-        <footer className="footer text-center lg:text-left text-white mt-3 bg-[#040335]">
-            <div className="mx-6 py-10 text-center md:text-left">
-                <div className="grid grid-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <footer className="flex flex-col w-full mt-3 bg-[#040335]">
+            <div className="mx-6 py-10 text-center md:text-left text-white">
+                <div className="grid grid-1 md:grid-cols-2 md:grid-cols-4 gap-8">
+                    
                         <div className="">
-                            <div className="logo w-16 ml-32">
-                                <img src={LogoFooter} alt="" />
-                            </div>
-                                <p className="mt-4 text-center">© Copyright 2022</p>
-                                <p className="text-center">DEVsafio</p>
+                            <img src={LogoFooter} className="w-14 lg:w-16 mx-auto" alt=""/>
+                            <p className="mt-4 text-sm text-center">© Copyright 2022</p>
+                            <p className="text-sm text-center">DEVsafio</p>
                         </div>
                         
-                     
-                        <div className="Nosotros font-medium text-sm">
-                            <h6 className="uppercase font-semibold mb-3 ml-40 flex justify-center md:justify-start">Conócenos</h6>
-
-                            <p className="flex items-center justify-center ml-40 md:justify-start mb-3">¿Quienes somos?</p>
-
-                            <p className="flex items-center justify-center ml-40 md:justify-start mb-3">Noticias</p>
-
-                            <p className="flex items-center justify-center ml-40 md:justify-start mb-3">Trabaja con Nosotros</p>
+                        <div className="Nosotros hidden lg:block font-medium text-sm">
+                            <h6 className="uppercase font-semibold mb-3 ml-40 flex lg:justify-start">Conócenos</h6>
+                            <p className="flex items-center ml-40 lg:justify-start mb-3">¿Quienes somos?</p>
+                            <p className="flex items-center ml-40 lg:justify-start mb-3">Noticias</p>
+                            <p className="flex items-center ml-40 lg:justify-start mb-3">Trabaja con Nosotros</p>
                         </div>
 
-                        <div className="Contacto font-medium text-sm">
-                            <h6 className="uppercase font-semibold mb-3 ml-20 flex justify-center md:justify-start">Contacto</h6>
-
-                            <p className="flex items-center justify-center ml-20 md:justify-start mb-3">Dirección - Chile</p>
-
-                            <p className="flex items-center justify-center ml-20 md:justify-start mb-3">info@example.com</p>
-
-                            <p className="flex items-center justify-center ml-20 md:justify-start mb-3">Phono</p>
+                        <div className="Contacto hidden lg:block font-medium text-sm">
+                            <h6 className="uppercase font-semibold mb-3 ml-20 flex ">Contacto</h6>
+                            <p className="flex items-center ml-20 lg:justify-start mb-3">Dirección - Chile</p>
+                            <p className="flex items-center ml-20 lg:justify-start mb-3">info@example.com</p>
+                            <p className="flex items-center ml-20 md:justify-start mb-3">Phono</p>
                         </div>
 
-                        <div className="Social font-medium text-sm">
+                        <div className="Social hidden lg:block font-medium text-sm">
                             <h6 className="uppercase font-semibold mb-3 flex justify-center md:justify-start">Social</h6>
-
                             <p className="mb-3">
                                 <a href="#!" className="text-white-600">Linkedin</a>
                             </p>
@@ -50,8 +41,6 @@ export function Footer() {
                                 <a href="#!" className="text-white-600">Facebook</a>
                             </p>
                         </div>
-                        
-                    
                 </div>
             </div>
             
--- src/components/Landing4.js
+++ src/components/Landing4.js
@@ -7,114 +7,66 @@ export function Landing4() {
 
     return (
 
-    <div className="">
+    <div className="flex flex-col">
 
-         <div className="flex justify-center mb-20">
+        <div className="hidden lg:block mx-auto my-24">
             <h1 className="text-5xl font-bold text-[#000000]">Empresas que confían en DEVsafio</h1>
         </div>
+   
+    {/* Slider */}
+        <div id="carouselExampleIndicators" className="hidden lg:block carousel slide relative" data-bs-ride="carousel">
 
-                     
-<div id="carouselExampleIndicators" class="carousel slide relative" data-bs-ride="carousel">
-  <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4 bg-[#000000]">
-    <button
-      type="button"
-      data-bs-target="#carouselExampleIndicators"
-      data-bs-slide-to="0"
-      class="active"
-      aria-current="true"
-      aria-label="Slide 1"
-    ></button>
-    <button
-      type="button"
-      data-bs-target="#carouselExampleIndicators"
-      data-bs-slide-to="1"
-      aria-label="Slide 2"
-    ></button>
-
-  </div>
-  <div class="carousel-inner relative w-full overflow-hidden">
-    
-        <div class="carousel-item active float-left w-full">
-            <div class="flex grid-cols-3 gap-20 justify-center mb-16">
-
-            <img
-                src={business1}
-                class="block w-44"
-                alt="Wild Landscape"
-            />
-
-            <img
-                src={business2}
-                class="block w-44"
-                alt="Wild Landscape"
-            />
-
-            <img
-                src={business1}
-                class="block w-44"
-                alt="Wild Landscape"
-            />
-
-            <img
-                src={business2}
-                class="block w-44"
-                alt="Wild Landscape"
-            />
+            <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4"> {/* indicator-sliders */}
+                <button type="button" data-bs-target="#carouselExampleIndicators"data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1">
+                </button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"> 
+                </button>
             </div>
+
+    {/* Imagen Slider */}
+                <div className="carousel-inner relative w-full overflow-hidden">
+
+                    <div className="carousel-item active float-left w-full">
+                        <div className="flex grid-cols-3 gap-20 justify-center mb-16">
+
+                            <img src={business1} className="block w-44" alt="Wild Landscape"/>
+                            <img src={business2} className="block w-44" alt="Wild Landscape"/>
+                            <img src={business1} className="block w-44" alt="Wild Landscape"/>
+                            <img src={business2} className="block w-44" alt="Wild Landscape"/>
+                        </div>
+                    </div>
+
+                    <div className="carousel-item float-left w-full">
+                        <div className="flex grid-cols-3 gap-20 justify-center mb-16">
+
+                            <img src={business2}className="block w-44" alt="Wild Landscape"/>
+                            <img src={business1}  className="block w-44" alt="Wild Landscape"/>
+                            <img src={business2} className="block w-44" alt="Wild Landscape"/>
+                            <img src={business1} className="block w-44" alt="Wild Landscape"/>
+                        </div>
+                    </div>
+
+                </div>
+
+        <button
+            className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev"
+        >
+            <span className="carousel-control-prev-icon inline-block bg-no-repeat]" aria-hidden="true"></span>  {/* flecha prev. slider */}
+            <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+            className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next"
+        >
+            <span className="carousel-control-next-icon inline-block bg-no-repeat]" aria-hidden="true"></span> {/* flecha next slider */}
+            <span className="visually-hidden">Next</span>
+        </button>
         </div>
-
-        <div class="carousel-item float-left w-full">
-            <div class="flex grid-cols-3 gap-20 justify-center mb-16">
-
-            <img
-                src={business2}
-                class="block w-44"
-                alt="Wild Landscape"
-            />
-
-            <img
-                src={business1}
-                class="block w-44"
-                alt="Wild Landscape"
-            />
-
-            <img
-                src={business2}
-                class="block w-44"
-                alt="Wild Landscape"
-            />
-
-            <img
-                src={business1}
-                class="block w-44"
-                alt="Wild Landscape"
-            />
-            </div>
-        </div>
-    
-  </div>
-  <button
-    class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-    type="button"
-    data-bs-target="#carouselExampleIndicators"
-    data-bs-slide="prev"
-  >
-    <span class="carousel-control-prev-icon inline-block bg-no-repeat bg-[#000000]" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button
-    class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-    type="button"
-    data-bs-target="#carouselExampleIndicators"
-    data-bs-slide="next"
-  >
-    <span class="carousel-control-next-icon inline-block bg-no-repeat bg-[#000000]" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-
-
-        
 
     </div>
             
