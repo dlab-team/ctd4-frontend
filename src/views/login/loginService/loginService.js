ab8302e2b1891e34c6a840170d31194496ebdcbe
david <david.aravenap@gmail.com>
2022-10-09 20:47:46 -0300

    ventana loading corregida con animacion de logo devsafio

--- src/views/login/loginService/loginService.js
+++ src/views/login/loginService/loginService.js
@@ -2,20 +2,23 @@ const axios = require('axios').default;
 
 export const loginService = (values, setResponseFromBack, setLoading) => {
     let url = 'http://localhost:3000/login/';
-    axios
-      .post(url, {
-        email: values.email,
-        password: values.password,
-      })
-      .then(function (response) {
-        if (response.data) {
-            window.localStorage.setItem("user", JSON.stringify(response.data));
-            setLoading(false)
-            window.location.reload();
-        }
-      })
-      .catch(function (error) {
-        setLoading(false)
-        setResponseFromBack(error.message);
-      });
+    setTimeout(() => {
+      
+      axios
+        .post(url, {
+          email: values.email,
+          password: values.password,
+        })
+        .then(function (response) {
+          if (response.data) {
+              window.localStorage.setItem("user", JSON.stringify(response.data));
+              setLoading(false)
+              window.location.reload();
+          }
+        })
+        .catch(function (error) {
+          setLoading(false)
+          setResponseFromBack(error.message);
+        });
+    }, 1500);
   }
