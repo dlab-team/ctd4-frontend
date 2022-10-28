0b2e542c82557793529ad81f69c071823ab9d319
david <david.aravenap@gmail.com>
2022-10-28 12:39:28 -0300

    refactor ultimo commit

--- src/views/Mapa/Mapa.js
+++ src/views/Mapa/Mapa.js
@@ -1,15 +1,26 @@
 import {MapaUI} from './views/MapaUI';
 import {CardGenerator} from './views/CardGenerator'
-import {TextCard} from './views/TextCard'
+import {Card} from './views/Card'
+import { useNavigate } from "react-router-dom";
+
 
 export const Mapa = () => {
+    const navigate = useNavigate();
+
+    const animationClickCard = (index, route) => {
+        document.getElementsByClassName("button")[index].style.animationDuration = "0.3s"
+        document.getElementsByClassName("button")[index].style.animationName = "pressButton"
+        setTimeout(() => {
+            navigate(route);
+        }, 300);
+    }
     return(
         <MapaUI>
-            <CardGenerator>
-                <TextCard href="/perfil" text={"Completa tu perfil de usuario"} />
-                <TextCard href="/datospersonales" text={"Realiza tests tecnicos"} />
-                <TextCard href="/perfileducacional" text={"Participa en show de codes"} />
-                <TextCard href="/perfileducacional" text={"Recibe ofertas de trabajo"} />
+            <CardGenerator animationClickCard={animationClickCard}>
+                <Card href="/perfil" text={"Completa tu perfil de usuario"} />
+                <Card href="/datospersonales" text={"Realiza tests tecnicos"} />
+                <Card href="/perfileducacional" text={"Participa en show de codes"} />
+                <Card href="/perfileducacional" text={"Recibe ofertas de trabajo"} />
             </CardGenerator>
         </MapaUI>
     )
