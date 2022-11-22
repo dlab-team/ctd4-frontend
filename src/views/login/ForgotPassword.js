a78c78ae4eb74d09ee3f1206deb31d4499a1ffd3
andrelealr <100944239+andrelealr@users.noreply.github.com>
2022-11-22 17:43:53 -0300

    Reemplazo checkbox formulario grande

---
+++ src/views/login/ForgotPassword.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { Link } from 'react-router-dom';
+
+export const ForgotPassword() {
+    return (
+        <div>
+            <h1>Forgot Password</h1>
+        </div>
+    )
+}
