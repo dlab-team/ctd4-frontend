8bd6b4610ea25967831230c036dac99eec0a6329
Carlos Araya Salfate <carlosarayasalfate@gmail.com>
2022-12-03 14:36:43 -0300

    añadi el nombre al dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -73,7 +73,7 @@ export default function Dropdown() {
               <Menu.Item>
                 {({ active }) => (
                   <a href="#" className={classNames(active ? 'bg-gray-100 text-gray-300' : 'text-gray-700', 'block px-4 py-2 text-sm font-medium')}>
-                   {user.status}
+                   {user.fullname} {user.status}
                   </a>
                   
                 )}
