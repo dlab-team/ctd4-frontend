4c82b69d839b77832638967d805f1b2042268518
Antoni <antoni.protoni@gmail.com>
2022-12-06 13:34:03 -0300

    fix infinite-loop

--- src/views/TestTecnicos/Modal/FormModalSkills.jsx
+++ src/views/TestTecnicos/Modal/FormModalSkills.jsx
@@ -1,53 +1,53 @@
-import { useState, useEffect } from "react"
-import { Form, Formik } from "formik"
-import  InputTest  from "../Componets/InputTest"
-import axios from "axios"
+import { useState, useEffect } from 'react';
+import { Form, Formik } from 'formik';
+import InputTest from '../Componets/InputTest';
+import axios from 'axios';
 
-const FormModalSkills = () => {
+const FormModalSkills = ({ setAuxTag }) => {
+  const [tag, setTag] = useState([]);
 
-    const [tag, setTag] = useState([]);
+  useEffect(() => {
+    axios.get(process.env.REACT_APP_BACKEND_URL + '/labels').then((res) => {
+      const datos = res.data;
+      setTag(datos);
+    });
+  }, []);
 
-    useEffect(()=>{
-        axios.get(process.env.REACT_APP_BACKEND_URL + '/labels').then((res) => {
-            const datos = res.data;
-            setTag(datos);
-          });
-    },[tag])
+  return (
+    <>
+      <div>
+        <Formik
+          initialValues={{
+            name: '',
+          }}
+          onSubmit={(values) => {
+            let url = process.env.REACT_APP_BACKEND_URL + '/labels';
+            axios
+              .post(url, {
+                name: values.name,
+              })
+              .then(function (response) {
+                setAuxTag(response);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+          }}
+        >
+          <Form>
+            <div className='text-center'>
+              <InputTest name='name' label='Agregar tag' />
+              <div>
+                <button type='submit' className='btn'>
+                  Guardar
+                </button>
+              </div>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+    </>
+  );
+};
 
-    return(
-        <>
-        <div>
-            <Formik
-            initialValues={{
-                name: '',
-            }}
-            onSubmit={(values) =>{
-                let url = process.env.REACT_APP_BACKEND_URL + '/labels'
-                axios.post(url, {
-                    name: values.name
-                })
-                .then(function (response) {
-                    console.log(response);
-                  })
-                  .catch(function (error) {
-                    console.log(error);
-                  });
-            }}
-            >
-                <Form>
-                    <div className="text-center"> 
-
-                    <InputTest name='name' label='Agregar tag' />                 
-                        <div>
-                            <button type='submit' className='btn'>Guardar</button>
-                        </div>
-                    </div>
-                </Form>
-            </Formik>
-        </div>
-
-        </>
-    )
-}
-
-export default FormModalSkills
+export default FormModalSkills;
--- src/views/TestTecnicos/TestTecnicos.jsx
+++ src/views/TestTecnicos/TestTecnicos.jsx
@@ -13,77 +13,75 @@ import FormModalQuizzes from './Modal/FormModalQuizzes';
 import ModalQuizzes from './Modal/ModalQuizzes';
 import axios from 'axios';
 
-
 export function TestTecnicos() {
-
-  const [showModal, setShowModal ] = useState(false);
-  const [showModalQuizzes, setShowModalQuizzes ] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [showModalQuizzes, setShowModalQuizzes] = useState(false);
   const [tag, setTag] = useState([]);
-  const [newItem, setNewItem] = useState("");
+  const [newItem, setNewItem] = useState('');
   const [quizzes, setQuizzes] = useState([]);
 
-  useEffect(()=> {
+  const [auxTag, setAuxTag] = useState('');
+  const [auxq, setAuxq] = useState([]);
+
+  useEffect(() => {
     axios.get(process.env.REACT_APP_BACKEND_URL + '/labels').then((res) => {
       const datos = res.data;
       setTag(datos);
-    }); 
-  },[tag])
+    });
+  }, [auxTag]);
 
-  useEffect(()=> {
+  useEffect(() => {
     axios.get(process.env.REACT_APP_BACKEND_URL + '/quizzes').then((res) => {
       const datos = res.data;
       setQuizzes(datos);
-    }); 
-  },[])
+    });
+  }, [auxq]);
 
-  
-
-  const onClickDelete = (id) =>{
-
-    const urlDelete = `http://localhost:3000/labels/${id}`
-      axios.delete(urlDelete)
-      .then((response)=>{
-        const res = response.data
+  const onClickDelete = (id) => {
+    const urlDelete = `http://localhost:3000/labels/${id}`;
+    axios
+      .delete(urlDelete)
+      .then((response) => {
+        const res = response.data;
         if (res.success) {
-          const newItems = [...tag]
-          newItems.splice(id,1)
-          setTag(newItems)
+          const newItems = [...tag];
+          newItems.splice(id, 1);
+          setTag(newItems);
         }
       })
-      .catch(error=>{
-        alert("Error ==> "+error)
-      })
-  }
+      .catch((error) => {
+        alert('Error ==> ' + error);
+      });
+  };
 
-  const onClickDeleteQuizz = (id) =>{
-
-    const urlDelete = `http://localhost:3000/quizzes/${id}`
-      axios.delete(urlDelete)
-      .then((response)=>{
-        const res = response.data
+  const onClickDeleteQuizz = (id) => {
+    const urlDelete = `http://localhost:3000/quizzes/${id}`;
+    axios
+      .delete(urlDelete)
+      .then((response) => {
+        const res = response.data;
         if (res.success) {
-          const newItems = [...tag]
-          newItems.splice(id,1)
-          setTag(newItems)
+          const newItems = [...tag];
+          newItems.splice(id, 1);
+          setTag(newItems);
         }
       })
-      .catch(error=>{
-        alert("Error ==> "+error)
-      })
-  }
-
+      .catch((error) => {
+        alert('Error ==> ' + error);
+      });
+  };
 
   return (
     <>
-    <ModalSkills
-    isVisible={showModal} onClose={() => setShowModal(false)}
-    >
-      <FormModalSkills />
-    </ModalSkills>
-    <ModalQuizzes isVisible={showModalQuizzes} onClose={() => setShowModal(false)}>
-
-      <FormModalQuizzes />
-    </ModalQuizzes>
+      <ModalSkills isVisible={showModal} onClose={() => setShowModal(false)}>
+        <FormModalSkills setAuxTag={setAuxTag} />
+      </ModalSkills>
+      <ModalQuizzes
+        isVisible={showModalQuizzes}
+        onClose={() => setShowModal(false)}
+      >
+        <FormModalQuizzes />
+      </ModalQuizzes>
       <div className='flex flex-col'>
         <NavPerfil />
         <div className='flex'>
@@ -106,11 +104,13 @@ export function TestTecnicos() {
               Tip: Si temes fallar un test, no te preocupes. Puedes volver a
               realizarlo luego de 3 meses.
             </p>
-            <div className="flex justify-between">
-            <h3 className='text-xl font-semibold mt-6'>Habilidades</h3>
-            <button onClick={() => setShowModal(true)} className='btn '>Agregar Tag</button>
+            <div className='flex justify-between'>
+              <h3 className='text-xl font-semibold mt-6'>Habilidades</h3>
+              <button onClick={() => setShowModal(true)} className='btn '>
+                Agregar Tag
+              </button>
             </div>
-            
+
             <input
               type='text'
               className=' bg-[#E2F2FE] border border-zinc-300 mt-4 w-[36rem] h-[2.8rem] rounded-lg p-4'
@@ -121,58 +121,61 @@ export function TestTecnicos() {
               <button className='border  py-1 px-2 rounded-lg bg-blue-700 text-zinc-100'>
                 {' '}
                 Todos{' '}
-              </button>
-              
-                {' '}
-                {tag.map((item, i)=>{
-                  return(
-                    <>
-                    <div className="flex flex-row">
-                      <span className='border border-blue-700 py-1 px-2 rounded-lg hover:bg-blue-700 hover:text-zinc-100' key={i}>
-                          {item.name}
+              </button>{' '}
+              {tag.map((item, i) => {
+                return (
+                  <>
+                    <div className='flex flex-row'>
+                      <span
+                        className='border border-blue-700 py-1 px-2 rounded-lg hover:bg-blue-700 hover:text-zinc-100'
+                        key={i}
+                      >
+                        {item.name}
                       </span>
-                      <button onClick={()=> onClickDelete(item.id)}   className=' hover:visible text-transparent hover:text-[#1E1E1E] hover:bg-white hover:transition-all rounded-xl 
-                      ' key={i}>
-                          X
+                      <button
+                        onClick={() => onClickDelete(item.id)}
+                        className=' hover:visible text-transparent hover:text-[#1E1E1E] hover:bg-white hover:transition-all rounded-xl 
+                      '
+                        key={i}
+                      >
+                        X
                       </button>
                     </div>
-                    </>
-                  )
-                })}{' '}
+                  </>
+                );
+              })}{' '}
             </div>
 
             <h3 className='text-xl font-semibold mt-6'>Test Técnicos</h3>
-            <button onClick={() => setShowModalQuizzes(true)} className='btn '>Agregar Test</button>
-            
+            <button onClick={() => setShowModalQuizzes(true)} className='btn '>
+              Agregar Test
+            </button>
 
             <div className='cards mt-12 mb-24 flex gap-4'>
-            
-                {
-                  quizzes.map((item, i)=>{
-                    return(
-                      <>
-                      <div key={i}>
-                        <CardQuizzes 
+              {quizzes.map((item, i) => {
+                return (
+                  <>
+                    <div key={i}>
+                      <CardQuizzes
                         image={item.url_logo}
                         titulo={item.name}
                         text1={`Duración: ${item.duration}`}
                         text2='Iniciar test'
-                        />
-                        <button 
+                      />
+                      <button
                         className='hover:visible text-transparent hover:text-[#1E1E1E] hover:bg-white hover:transition-all rounded-xl'
-                        onClick={()=> onClickDeleteQuizz(item.id)}
-                        >
-                        X</button>
-                      </div>
-                      </>
-                    )
-                  })
-                } 
-              </div>
+                        onClick={() => onClickDeleteQuizz(item.id)}
+                      >
+                        X
+                      </button>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
           </div>
         </div>
         <Footer />
-
       </div>
     </>
   );
